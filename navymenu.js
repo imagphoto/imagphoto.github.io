e7180a104312ed5718bb7b24da7991d27d96d83c
imagphoto <imagphotoblogspot@gmail.com>
2019-12-31 18:41:43 +0530

    Create navymenu.js

---
+++ navymenu.js
@@ -0,0 +1,8 @@
+function mFunction() {
+  var x = document.getElementById(&quot;mnavybary&quot;);
+  if (x.className === &quot;navybary&quot;) {
+    x.className += &quot; responsive&quot;;
+  } else {
+    x.className = &quot;navybary&quot;;
+  }
+}
