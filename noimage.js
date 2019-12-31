c978bab74f7e391d7d1ae4f56dddcb25ba46b034
imagphoto <imagphotoblogspot@gmail.com>
2019-12-31 17:05:51 +0530

    Create noimage.js

---
+++ noimage.js
@@ -0,0 +1,10 @@
+//<![CDATA[
+var no_image = "http://1.bp.blogspot.com/-PpEYvGHaGG4/W4EVeRjE-ZI/AAAAAAAAw7I/tJ8O9i-0yX8SdgcMUnAPiQmukn8m2SRiACK4BGAYYCw/s1600/ethanthii%2Blogo.jpg";
+var month_format = [, "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sept", "Oct", "Nov", "Dec"];
+var more_text = "View More";
+var postperpage = 8;
+var related_number = 6;
+var comments_text = "<span>Post </span>Comment";
+var pagenav_prev = "Previous";
+var pagenav_next = "Next";
+//]]>
